875989317000d8875d6067f001495fb343420a95
zuisong <com.me@foxmail.com>
2020-01-28 21:12:30 +0800

    update

--- tests/test-json-parser.ts
+++ tests/test-json-parser.ts
@@ -1,7 +1,8 @@
-import { deepStrictEqual } from 'assert';
-import { generateAST, generateObject, generateTokes } from '../index';
+import { generateAST, generateObject, generateTokes } from "../index";
+import { deepStrictEqual } from "assert";
 
 // 原本的json串
+// language=json
 const a = `
 {
     c: -11,
@@ -15,52 +16,48 @@ const a = `
 `;
 
 // 预期得到的对象
-const expectObject =
-{
-    c: -11,
-    d: [
-        1,
-        {
-            f: 'g'
-        }
-    ]
+const expectObject = {
+  c: -11,
+  d: [
+    1,
+    {
+      f: "g"
+    }
+  ]
 };
 
 // 预期得的tokens
-const expectTokens =
-    [
-        { type: '大括号', value: '{' },
-        { type: '名字', value: 'c' },
-        { type: '冒号', value: ':' },
-        { type: '数字', value: '-11' },
-        { type: '逗号', value: ',' },
-        { type: '名字', value: 'd' },
-        { type: '冒号', value: ':' },
-        { type: '方括号', value: '[' },
-        { type: '数字', value: '1' },
-        { type: '逗号', value: ',' },
-        { type: '大括号', value: '{' },
-        { type: '名字', value: 'f' },
-        { type: '冒号', value: ':' },
-        { type: '字符串', value: 'g' },
-        { type: '大括号', value: '}' },
-        { type: '方括号', value: ']' },
-        { type: '大括号', value: '}' },
-    ];
-
-
+const expectTokens = [
+  { type: "大括号", value: "{" },
+  { type: "名字", value: "c" },
+  { type: "冒号", value: ":" },
+  { type: "数字", value: "-11" },
+  { type: "逗号", value: "," },
+  { type: "名字", value: "d" },
+  { type: "冒号", value: ":" },
+  { type: "方括号", value: "[" },
+  { type: "数字", value: "1" },
+  { type: "逗号", value: "," },
+  { type: "大括号", value: "{" },
+  { type: "名字", value: "f" },
+  { type: "冒号", value: ":" },
+  { type: "字符串", value: "g" },
+  { type: "大括号", value: "}" },
+  { type: "方括号", value: "]" },
+  { type: "大括号", value: "}" }
+];
 
 const tokens = generateTokes(a);
 
-deepStrictEqual(tokens, expectTokens, 'token 转换不符合预期')
+deepStrictEqual(tokens, expectTokens, "token 转换不符合预期");
 
-const ast = generateAST(tokens)
+const ast = generateAST(tokens);
 
-const obj = generateObject(ast)
+const obj = generateObject(ast);
 
-deepStrictEqual(obj, expectObject, '对象转换不对')
+deepStrictEqual(obj, expectObject, "对象转换不对");
 
 console.log(a);
 console.log(obj);
 
-console.log("OK!!! All down!!!")
+console.log("OK!!! All down!!!");
